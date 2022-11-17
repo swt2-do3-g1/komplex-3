b625246a1b7a179b1e5cd239aa5bb7faa5f578f0
Nick <nick.ruider@gmail.com>
2022-11-17 12:32:38 +0100

    changes to junit

--- src/test/java/org/swt2/AppTest.java
+++ src/test/java/org/swt2/AppTest.java
@@ -1,12 +1,14 @@
 package org.swt2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class AppTest {
     @Test
     void test() {
-        assertEquals(true, true);
+        assertTrue(true);
     }
 
 }
