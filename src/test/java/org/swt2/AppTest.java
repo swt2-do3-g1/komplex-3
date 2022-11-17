3a6ed75bb34c3c96bf3f70bd5e4d270f29480ab3
Nick <nick.ruider@gmail.com>
2022-11-17 11:38:43 +0100

    added junit5 added spotbugs

--- src/test/java/org/swt2/AppTest.java
+++ src/test/java/org/swt2/AppTest.java
@@ -1,38 +1,12 @@
-package org.swt2-do3-g1;
+package org.swt2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+class AppTest {
+    @Test
+    void test() {
+        assertEquals(true, true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
